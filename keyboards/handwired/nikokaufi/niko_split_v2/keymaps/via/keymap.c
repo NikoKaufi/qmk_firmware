854ab7c1388607297eb5d738565fbcedce9fc5bf
Kaufmann <kaufmanj@medads.uk-erlangen.de>
2024-09-24 15:52:05 +0200

    adjust keymap via

--- keyboards/handwired/nikokaufi/niko_split_v2/keymaps/via/keymap.c
+++ keyboards/handwired/nikokaufi/niko_split_v2/keymaps/via/keymap.c
@@ -66,104 +66,93 @@ tap_dance_action_t tap_dance_actions[] = {
 /* KEYMAP */
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*  [0]
-     * ┌───┬───┬───┬───┬───┬───┐                     ┌───┬───┬───┬───┬───┬───┐
-     * │ES4│ Q │ W │ E │ R │ T |                     | Z | U | I | O | P | Ü |
-     * ├───┼───┼───┼───┼───┼───┤                     |───┼───┼───┼───┼───┼───┤
-     * │TAB│ A │ S │ D │ F │ G |                 ┌───┤ H | J | K | L | Ö | Ä |
-     * ├───┼───┼───┼───┼───┼───┼───┐             |PLP├───┼───┼───┼───┼───┼───┤
-     * │<|>│ Y │ X │ C │ V | B |MUT|             └───┤ N | M | , | . | - |SHI|
-     * └───┴───┴───┴───┼───┼───┼───┼───┐    ┌───┬───┬┴───┴┬──┴┬──┴───┴───┴───┘
-     *                 |CTL|SPC|ALT|WIN|    |ALT|BSP| SPC |ENT|
-     *                 └───┴───┴───┴───┘    └───┴───┴─────┴───┘
+     * ┌───┬───┬───┬───┬───┬───┐            ┌───┬───┬───┬───┬───┬───┐
+     * │ ? │ Q │ W │ E │ R │ T |            | Z | U | I | O | P | Ü |
+     * ├───┼───┼───┼───┼───┼───┤            |───┼───┼───┼───┼───┼───┤
+     * │ ? │ A │ S │ D │ F │ G |            | H | J | K | L | Ö | Ä |
+     * ├───┼───┼───┼───┼───┼───┼───┐    ┌───┼───┼───┼───┼───┼───┼───┤
+     * │ ? │ Y │ X │ C │ V | B |MUT|    |PLP| N | M | , | . | - | ? |
+     * └───┴───┴───┴───┼───┼───┼───┤    ├───┼───┼───┼───┴───┴───┴───┘
+     *                 | ? | ? | ? |    | ? | ? | ? |
+     *                 └───┴───┴───┘    └───┴───┴───┘
      */
     [0] = LAYOUT_custom(
-        ESC_AF4, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_DEL,
-        KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_F13,
+        ESC_AF4, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,
+        KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,
         KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_MUTE,
-                                            KC_LCTL, SPC1,    ALT2,    KC_LGUI,
+                                            KC_LCTL, SPC1,    ALT2,
         //right
-                 KC_HOME, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,
-                 KC_END,  KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, QUO_SHI,
-                 KC_MPLY, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-        KC_RALT, KC_BSPC, SPC3,    ENT_CTL
+                 KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,
+                 KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, QUO_SHI,
+        KC_MPLY, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+        KC_RALT, KC_BSPC, SPC3
         ),
     /*  [1]
-     * ┌───┬───┬───┬───┬───┬───┐                     ┌───┬───┬───┬───┬───┬───┐
-     * │^° │F1 │F2 │F3 │F4 │F5 ├───┐             ┌───┤F6 |F7 |F8 |F9 |F10|´` |
-     * ├───┼───┼───┼───┼───┼───┤INS│             |PUP├───┼───┼───┼───┼───┼───┤
-     * │   │F11│F12│MS↑│   │   ├───┤             ├───┤   |   | ↑ |   |   |+*~|
-     * ├───┼───┼───┼───┼───┼───┤F14│             |PDN├───┼───┼───┼───┼───┼───┤
-     * │SHI│   │MS←│MS↓│MS→│   ├───┤             ├───┤   | ← | ↓ | → |   |#' |
-     * ├───┼───┼───┼───┼───┼───┤MS1│             |MS2├───┼───┼───┼───┼───┼───┤
-     * │CAP│   │   │   │   |MS1├───┘             └───┤NKR|   |   |   |   |   |
-     * └───┴───┴───┴──┬┴──┬┴───┴┬───┬───┐   ┌───┬───┬┴───┴┬──┴┬──┴───┴───┴───┘
-     *                |CTL| /1\ |ALT|WIN|   |ALT|BSP| WIN |CTL|
-     *                └───┴─────┴───┴───┘   └───┴───┴─────┴───┘
+     * 
+     * ┌───┬───┬───┬───┬───┬───┐            ┌───┬───┬───┬───┬───┬───┐
+     * │   │F11│F12│MS↑│   │   |            |   |   | ↑ |   |   |+*~|
+     * ├───┼───┼───┼───┼───┼───┤            ├───┼───┼───┼───┼───┼───┤
+     * │SHI│   │MS←│MS↓│MS→│   |            |   | ← | ↓ | → |   |#' |
+     * ├───┼───┼───┼───┼───┼───┼───┐    ┌───┼───┼───┼───┼───┼───┼───┤
+     * │CAP│   │   │   │   |MS1| ? |    |   |NKR|   |   |   |   |   |
+     * └───┴───┴───┴───┼───┼───┼───┤    ├───┼───┼───┼───┴───┴───┴───┘
+     *                 |   |   |   |    |   |   |   |
+     *                 └───┴───┴───┘    └───┴───┴───┘
      */
     [1] = LAYOUT_custom(
-        KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,
-        KC_NO,   KC_F11,  KC_F12,  KC_MS_U, KC_NO,   KC_NO,   KC_INS,
-        KC_LSFT, KC_NO,   KC_MS_L, KC_MS_D, KC_MS_R, KC_NO,   KC_F14,
+        KC_NO,   KC_F11,  KC_F12,  KC_MS_U, KC_NO,   KC_NO,
+        KC_LSFT, KC_NO,   KC_MS_L, KC_MS_D, KC_MS_R, KC_NO,
         KC_CAPS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_BTN1, KC_BTN1,
-                                            KC_LCTL, _______, KC_LALT, KC_LGUI,
+                                            KC_LCTL, _______, KC_LALT,
         //right
-                          KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_EQL,
-                 KC_PGUP, KC_NO,   KC_NO,   KC_UP,   KC_NO,   KC_NO,   KC_RBRC,
-                 KC_PGDN, KC_NO,   KC_LEFT, KC_DOWN, KC_RGHT, KC_NO,   KC_BSLS,
-                 KC_BTN2, NK_TOGG, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-        KC_RALT, KC_BSPC, KC_RGUI, KC_RCTL
+                 KC_NO,   KC_NO,   KC_UP,   KC_NO,   KC_NO,   KC_RBRC,
+                 KC_NO,   KC_LEFT, KC_DOWN, KC_RGHT, KC_NO,   KC_BSLS,
+        KC_BTN2, NK_TOGG, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+        KC_RALT, KC_BSPC, KC_RGUI
         ),
     /*  [2]
-     * ┌───┬───┬───┬───┬───┬───┐                     ┌───┬───┬───┬───┬───┬───┐
-     * │OFF│STP│   │   │   │   ├───┐             ┌───┤   |   |   |   |   |ß?\|
-     * ├───┼───┼───┼───┼───┼───┤   │             |   ├───┼───┼───┼───┼───┼───┤
-     * │   │   │   │   │   │   ├───┤             ├───┤   |   |   |   |   |+*~|
-     * ├───┼───┼───┼───┼───┼───┤   │             |   ├───┼───┼───┼───┼───┼───┤
-     * │SHI│ 1 │ 2 │ 3 │ 4 │ 5 ├───┤             ├───┤ 6 | 7 | 8 | 9 | 0 |#' |
-     * ├───┼───┼───┼───┼───┼───┤   │             |   ├───┼───┼───┼───┼───┼───┤
-     * │   │   │   │   │   |   ├───┘             └───┤   |   |,; |.: |-_ |   |
-     * └───┴───┴───┴──┬┴──┬┴───┴┬───┬───┐   ┌───┬───┬┴───┴┬──┴┬──┴───┴───┴───┘
-     *                |   |     |/2\|   |   |ALT|BSP| SHI |CTL|
-     *                └───┴─────┴───┴───┘   └───┴───┴─────┴───┘
+     * ┌───┬───┬───┬───┬───┬───┐            ┌───┬───┬───┬───┬───┬───┐
+     * │F1 │F2 │F3 │F4 │F5 │F6 |            |F7 |F8 |F9 |F10|F11|F12|
+     * ├───┼───┼───┼───┼───┼───┤            ├───┼───┼───┼───┼───┼───┤
+     * │SHI│ 1 │ 2 │ 3 │ 4 │ 5 |            | 6 | 7 | 8 | 9 | 0 |#' |
+     * ├───┼───┼───┼───┼───┼───┼───┐    ┌───┼───┼───┼───┼───┼───┼───┤
+     * │   │   │   │   │   |   |   |    |   |   |,; |.: |-_ |   |   |
+     * └───┴───┴───┴───┼───┼───┼───┤    ├───┼───┼───┼───┴───┴───┴───┘
+     *                 |   |   |   |    |   |   |   |
+     *                 └───┴───┴───┘    └───┴───┴───┘
      */
     [2] = LAYOUT_custom(
-        OFF,     STP,     KC_NO,   KC_NO,   KC_NO,   KC_NO,
+        KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,
+        KC_LSFT, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,
         KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-        KC_LSFT, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_NO,
-        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-                                            KC_NO,   KC_NO,   _______, KC_NO,
+                                            KC_NO,   KC_NO,   KC_NO,
         //right
-                          KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_MINS,
-                 KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_RBRC,
-                 KC_NO,   KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSLS,
-                 KC_NO,   KC_NO,   KC_NO,   KC_COMM, KC_DOT,  KC_SLSH, KC_NO,
-        KC_RALT, KC_BSPC, KC_RSFT, KC_RCTL
+                 KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
+                 KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSLS,
+        KC_NO,   KC_NO,   KC_COMM, KC_DOT,  KC_SLSH, KC_NO,   KC_NO,
+        KC_NO,   KC_NO,   KC_NO,   KC_NO
         ),
     /*  [3]
-     * ┌───┬───┬───┬───┬───┬───┐                     ┌───┬───┬───┬───┬───┬───┐
-     * │   │   │   │   │   │   ├───┐             ┌───┤   |   |   |   |   |   |
-     * ├───┼───┼───┼───┼───┼───┤   │             |   ├───┼───┼───┼───┼───┼───┤
-     * │TOG│MOD│VAI│HUI│SAI│   ├───┤             ├───┤   |   |   |   |   |   |
-     * ├───┼───┼───┼───┼───┼───┤   │             |   ├───┼───┼───┼───┼───┼───┤
-     * │   │RMO│VAD│HUD│SAD│   ├───┤             ├───┤   |   |   |   |   |   |
-     * ├───┼───┼───┼───┼───┼───┤   │             |   ├───┼───┼───┼───┼───┼───┤
-     * │   │   │   │   │   |   ├───┘             └───┤   |   |   |   |   |   |
-     * └───┴───┴───┴──┬┴──┬┴───┴┬───┬───┐   ┌───┬───┬┴───┴┬──┴┬──┴───┴───┴───┘
-     *                |   |     |   |   |   |   |   | /3\ |   |
-     *                └───┴─────┴───┴───┘   └───┴───┴─────┴───┘
+     * ┌───┬───┬───┬───┬───┬───┐            ┌───┬───┬───┬───┬───┬───┐
+     * │TOG│MOD│VAI│HUI│SAI│   |            |   |   |   |   |   |   |
+     * ├───┼───┼───┼───┼───┼───┤            ├───┼───┼───┼───┼───┼───┤
+     * │   │RMO│VAD│HUD│SAD│   |            |   |   |   |   |   |   |
+     * ├───┼───┼───┼───┼───┼───┼───┐    ┌───┼───┼───┼───┼───┼───┼───┤
+     * │   │   │   │   │   |   |   |    |   |   |   |   |   |   |   |
+     * └───┴───┴───┴───┼───┼───┼───┤    ├───┼───┼───┼───┴───┴───┴───┘
+     *                 |   |   |   |    |   |   |   |
+     *                 └───┴───┴───┘    └───┴───┴───┘
      */
     [3] = LAYOUT_custom(
-        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-        RGB_TOG, RGB_MOD, RGB_VAI, RGB_HUI, RGB_SAI, KC_NO,   KC_NO,
-        KC_NO,   RGB_RMOD,RGB_VAD, RGB_HUD, RGB_SAD, KC_NO,   KC_NO,
+        RGB_TOG, RGB_MOD, RGB_VAI, RGB_HUI, RGB_SAI, KC_NO,
+        KC_NO,   RGB_RMOD,RGB_VAD, RGB_HUD, RGB_SAD, KC_NO,
         KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-                                            KC_NO,   KC_NO,   KC_NO,   KC_NO,
+                                            KC_NO,   KC_NO,   KC_NO,
         //right
-                          KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-                 KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-                 KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-                 KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-        KC_NO,   KC_NO,   _______, KC_NO
+                 KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+                 KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+        KC_NO,   KC_NO,   KC_NO,   KC_NO
         )
 
 };
