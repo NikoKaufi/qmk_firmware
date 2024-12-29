bb79096335a16c7f761c99de4fa4bc64bf15138f
NikoKaufi <nikokaufi@gmail.com>
2024-12-29 22:42:32 +0100

    change RGB-effects, OLED öbabycat and Keymap-adjustments

--- keyboards/handwired/nikokaufi/niko_split_v2/keymaps/via/keymap.c
+++ keyboards/handwired/nikokaufi/niko_split_v2/keymaps/via/keymap.c
@@ -91,64 +91,64 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         ),
     /*  [1]
      * ┌───┬───┬───┬───┬───┬───┐            ┌───┬───┬───┬───┬───┬───┐
-     * │   │   │   │MS↑│   │   |            |   |   | ↑ |   |   |+*~|
+     * │GES│   │   │MS↑│   │PgU|            |PgD|   | ↑ |   |   |+*~|
      * ├───┼───┼───┼───┼───┼───┤            ├───┼───┼───┼───┼───┼───┤
-     * │   │SHI│MS←│MS↓│MS→│Po1|            |End| ← | ↓ | → |   |#' |
+     * │CAP│SHI│MS←│MS↓│MS→│Po1|            |End| ← | ↓ | → |SHI|#' |
      * ├───┼───┼───┼───┼───┼───┼───┐    ┌───┼───┼───┼───┼───┼───┼───┤
-     * │CAP│   │   │   │   |MS1|MS1|    |   |NKR|   |   |   |   |   |
+     * │   │   │   │   │   |MS1|MS1|    |MS2|NKR|   |MS3|   |   |   |
      * └───┴───┴───┴───┼───┼───┼───┤    ├───┼───┼───┼───┴───┴───┴───┘
-     *                 |   |/1\|   |    |AGR|Win|   |
+     *                 |CTL|/1\|ALT|    |AGR|<- |Win|
      *                 └───┴───┴───┘    └───┴───┴───┘
      */
     [_1_NAV] = LAYOUT_split_3x6_3(
-        KC_NO,   KC_NO,   KC_NO,   KC_MS_U, KC_NO,   KC_NO,
-        KC_NO,   KC_LSFT, KC_MS_L, KC_MS_D, KC_MS_R, KC_HOME,
-        KC_CAPS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_BTN1, KC_BTN1,
+        QK_GESC, KC_NO,   KC_NO,   KC_MS_U, KC_NO,   KC_PGUP,
+        KC_CAPS, KC_LSFT, KC_MS_L, KC_MS_D, KC_MS_R, KC_HOME,
+        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_BTN1, KC_BTN1,
                                             KC_LCTL, _______, KC_LALT,
         //right
-                 KC_NO,   KC_NO,   KC_UP,   KC_NO,   KC_NO,   DE_PLUS,
-                 KC_END,  KC_LEFT, KC_DOWN, KC_RGHT, KC_NO,   DE_HASH,
-        KC_BTN2, NK_TOGG, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+                 KC_PGDN, KC_NO,   KC_UP,   KC_NO,   KC_NO,   DE_PLUS,
+                 KC_END,  KC_LEFT, KC_DOWN, KC_RGHT, KC_RSFT, DE_HASH,
+        KC_BTN2, NK_TOGG, KC_NO,   KC_BTN2, KC_NO,   KC_NO,   KC_NO,
         KC_RALT, KC_BSPC, KC_RGUI
         ),
     /*  [2]
      * ┌───┬───┬───┬───┬───┬───┐            ┌───┬───┬───┬───┬───┬───┐
-     * │F1 │F2 │F3 │F4 │F5 │F6 |            |F7 |F8 |F9 |F10|F11|F12|
+     * |F12│F1 │F2 │F3 │F4 │F5 │            |F6 |F7 |F8 |F9 |F10|F11|
      * ├───┼───┼───┼───┼───┼───┤            ├───┼───┼───┼───┼───┼───┤
      * │TAB│ 1 │ 2 │ 3 │ 4 │ 5 |            | 6 | 7 | 8 | 9 | 0 |#' |
      * ├───┼───┼───┼───┼───┼───┼───┐    ┌───┼───┼───┼───┼───┼───┼───┤
-     * │   │   │   │   │   |   |   |    |   |   |,; |.: |-_ |   |   |
+     * │   │   │   │   │   |   |   |    |   |   |   |,; |.: |-_ |   |
      * └───┴───┴───┴───┼───┼───┼───┤    ├───┼───┼───┼───┴───┴───┴───┘
-     *                 |   |   |/2\|    |AGR|<- |Win|
+     *                 |   |CTL|/2\|    |AGR|<- |Win|
      *                 └───┴───┴───┘    └───┴───┴───┘
      */
     [_2_NUM] = LAYOUT_split_3x6_3(
-        KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,
+        KC_F12,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,
         KC_TAB,  ONE_S,   KC_2,    KC_3,    KC_4,    KC_5,
         KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-                                            KC_NO,   KC_NO,   _______,
+                                            KC_NO,   KC_LCTL, _______,
         //right
-                 KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
+                 KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
                  KC_6,    KC_7,    KC_8,    KC_9,    ZER_S,   DE_HASH,
-        KC_NO,   KC_NO,   KC_COMM, KC_DOT,  DE_MINS, KC_NO,   KC_NO,
+        KC_NO,   KC_NO,   KC_NO,   KC_COMM, KC_DOT,  DE_MINS, KC_NO,
         KC_RALT, KC_BSPC, KC_RGUI
         ),
     /*  [3]
      * ┌───┬───┬───┬───┬───┬───┐            ┌───┬───┬───┬───┬───┬───┐
-     * │TOG│MOD│VAI│HUI│SAI│   |            |   |   |   |   |   |   |
+     * │TOG│MOU│VAU│HUU│SAU│SPU|            |   |   |   |   |   |   |
      * ├───┼───┼───┼───┼───┼───┤            ├───┼───┼───┼───┼───┼───┤
-     * │   │RMO│VAD│HUD│SAD│   |            |   |   |   |   |   |   |
+     * │   │RMO│VAD│HUD│SAD│SPD|            |   |   |   |   |   |   |
      * ├───┼───┼───┼───┼───┼───┼───┐    ┌───┼───┼───┼───┼───┼───┼───┤
-     * │   │   │   │   │   |   |   |    |   |   |   |   |   |   |   |
+     * │   │   │   │   │   |   |TOG|    |   |   |   |   |   |   |   |
      * └───┴───┴───┴───┼───┼───┼───┤    ├───┼───┼───┼───┴───┴───┴───┘
-     *                 |   |   |   |    |   |   |/3\|
+     *                 |STP|OFF|   |    |   |   |/3\|
      *                 └───┴───┴───┘    └───┴───┴───┘
      */
     [_3_RGB] = LAYOUT_split_3x6_3(
         RM_TOGG, RM_NEXT, RM_VALU, RM_HUEU, RM_SATU, RM_SPDU,
         KC_NO,   RM_PREV, RM_VALD, RM_HUED, RM_SATD, RM_SPDD,
-        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-                                            KC_NO,   KC_NO,   KC_NO,
+        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   RM_TOGG,
+                                            STP,     OFF,     KC_NO,
         //right
                  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
                  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
@@ -164,15 +164,15 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
     [_0_ALPHA] = {ENCODER_CCW_CW(KC_VOLD,  KC_VOLU),   ENCODER_CCW_CW(KC_MPRV, KC_MNXT)},
     [_1_NAV] =   {ENCODER_CCW_CW(KC_WH_L,  KC_WH_R),   ENCODER_CCW_CW(KC_WH_U, KC_WH_D)},
     [_2_NUM] =   {ENCODER_CCW_CW(KC_NO,    KC_NO),     ENCODER_CCW_CW(KC_NO,   KC_NO)},
-    [_3_RGB] =   {ENCODER_CCW_CW(KC_NO,    KC_NO),     ENCODER_CCW_CW(KC_NO,   KC_NO)}
+    [_3_RGB] =   {ENCODER_CCW_CW(RM_PREV,  RM_NEXT),   ENCODER_CCW_CW(KC_NO,   KC_NO)}
     };
 #endif //ENCODER_MAP
 
 /* Combo */
 #ifdef COMBO_ENABLE
-    const uint16_t PROGMEM abspc_del[] = {A_SHI, KC_BSPC, COMBO_END};
+    const uint16_t PROGMEM spc_bspc_del[] = {SPC1, KC_BSPC, COMBO_END};
     combo_t key_combos[] = {
-        COMBO(abspc_del, KC_DEL)
+        COMBO(spc_bspc_del, KC_DEL)
     };
 #endif //Combo
 
